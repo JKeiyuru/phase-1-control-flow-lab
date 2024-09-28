2063d0cf0c4abf22b232c183556b18f9103449e1
Joseph Keiyuru <josephkeiyuru1@gmail.com>
2024-09-28 17:15:11 +0300

    Conditionals lab

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400){
+    return 'This one is on me!'
+  } else if ( feet<= 2000){
+    return 'That will be twenty bucks.'
+  } else if ( feet> 2000 && feet<2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if ( feet> 2500)
+    return 'No can do.'
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let response;
+  city === 'NYC'? response='Ok, sounds good.' : response='No go.'
+  return response
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(charm){
   // Write your code here!
+  let answer;
+  switch (charm) {
+    case charm = 'generous':
+     answer = 'Thank you so much.'
+     break;
+    case charm = 'not as generous' :
+      answer = 'Thank you.'
+      break;
+    case charm = 'thanks for everything':
+        answer = 'Bye.'
+        break;
+    default :
+     console.log("Bye.");
+     break;
+  }
+  return answer;
 }
